658ccc24fae452ee2a7ba3e652699cb79d5302c1
Kaustubh Singh <singhkaustubh98@gmail.com>
2026-03-22 01:20:17 +0530

    Update ResumeTextExtractionService.java

--- src/main/java/com/resume2site/backend/resume/service/ResumeTextExtractionService.java
+++ src/main/java/com/resume2site/backend/resume/service/ResumeTextExtractionService.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +17,7 @@ public class ResumeTextExtractionService {
     public String extract(Path filePath) {
         try (InputStream inputStream = Files.newInputStream(filePath)) {
             return tika.parseToString(inputStream);
-        } catch (IOException exception) {
+        } catch (IOException | TikaException exception) {
             throw new ConflictException("Unable to extract text from the uploaded resume");
         }
     }
